9464b030687ac0b85fda51db15460eb7c77c9ffe
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-07-09 18:22:59 +0500

    home page fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,33 @@
 function Projects() {
   const sections = [
     { title: "Porject number 1:", width: "w-[300px]", height: "h-[400px]" },
-    { title: "Porject number 1:", width: "w-[400px]", height: "h-[300px]", margin:"mt-12" },
+    {
+      title: "Porject number 1:",
+      width: "w-[400px]",
+      height: "h-[300px]",
+      margin: "mt-12",
+    },
     { title: "Porject number 1:", width: "w-[400px]", height: "h-[400px]" },
     { title: "Porject number 1:", width: "w-[400px]", height: "h-[300px]" },
-    { title: "Porject number 1:", width: "w-[300px]", height: "h-[300px]", margin:"-mt-12 lg:mt-4"},
+    {
+      title: "Porject number 1:",
+      width: "w-[300px]",
+      height: "h-[300px]",
+      margin: "-mt-12 lg:mt-4",
+    },
   ];
 
-  
   return (
     <div className="bg-[#242735] h-auto text-white">
-      <div className="flex flex-wrap justify-evenly border">
-                {sections.map((section, index) => (
+      <div className="flex flex-wrap justify-evenly">
+        {sections.map((section, index) => (
           <div
             key={index}
             className={`border ${section.width} ${section.height} ${section.margin} mt-4 rounded-2xl flex justify-center items-center`}
           >
-            <h3 className="font-semibold text-center align-middle">{section.title}</h3>
+            <h3 className="font-semibold text-center align-middle">
+              {section.title}
+            </h3>
           </div>
         ))}
       </div>
