5423d7557e2cf3d69f3729db03a62aff36a61d02
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-07-09 09:32:32 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from './components/Contact';
 import Projects from './components/Projects';
-import './app.css'
 
 function App() {
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
