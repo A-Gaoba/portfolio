2d190f8390a216dd154b272ab79bbd82c20532e9
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-07-09 09:36:52 +0500

    fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
-import "../app.css";
 import {
   faTwitter,
   faLinkedin,
