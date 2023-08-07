d37081b137adc9e4e624a7ce744c08086a9b30c8
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-08-07 15:55:43 +0500

    done

--- src/components/work/ProjectsList.jsx
+++ src/components/work/ProjectsList.jsx
@@ -6,21 +6,21 @@ const ProjectsList = () => {
       title: 'To Russai',
       description: 'To Russai offers services to help students who aspire to study in Russian universities. The platform provides valuable resources and guidance to facilitate the application process, visa procedures, and university selection.',
       technologies: ['React', 'Tailwind CSS', 'API Integration'],
-      imageUrl: '../../../public/projectsImages/toru.png',
+      imageUrl: '/projectsImages/toru.png',
       link: "https://toru.netlify.app/"
     },
     {
       title: 'Ali Alnono',
       description: 'This project showcases the achievements and journey of Ali Alnono, a prominent football player from Yemen. It highlights his remarkable career, notable matches, and contributions to the sport in Yemen.',
       technologies: ['React', 'Tailwind CSS', 'Multimedia'],
-      imageUrl: '../../../public/projectsImages/alnono.png',
+      imageUrl: '/projectsImages/alnono.png',
       link: "https://alialnono.vercel.app/"
     },
     {
       title: 'GaobaVid - YouTube Clone',
       description: 'GaobaVid is a YouTube clone that replicates the core features of the original platform, allowing users to browse and watch videos, as well as upload and share their own content. The project demonstrates front-end development skills and API interaction.',
       technologies: ['React', 'Tailwind CSS', 'YouTube API'],
-      imageUrl: '../../../public/projectsImages/gaobavide.png',
+      imageUrl: '/projectsImages/gaobavide.png',
       link: "https://gaobavid.netlify.app/"
     },
   ];
