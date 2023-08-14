c881dd4ae099b73b72a3b36ad748573592f237fc
Abdulrahman Gaoba <abdulrahman1997abdo@gmail.com>
2023-08-14 21:57:09 +0500

    add two projects

--- src/components/work/ProjectsList.jsx
+++ src/components/work/ProjectsList.jsx
@@ -1,34 +1,54 @@
-import ProjectCard  from './ProjectCard';
+import ProjectCard from "./ProjectCard";
 import { motion } from "framer-motion";
 const ProjectsList = () => {
   const projects = [
     {
-      title: 'Sharaf Almahaqeri',
-      description: "Sharaf Almahaqeri's portfolio website showcases his skills, achievements, and information",
-      technologies: ['React', 'Tailwind CSS'],
-      imageUrl: '/projectsImages/sharaf.png',
-      link: "https://almahaqeri.vercel.app/"
+      title: "Sharaf Almahaqeri",
+      description:
+        "Sharaf Almahaqeri's portfolio website showcases his skills, achievements, and information",
+      technologies: ["React", "Tailwind CSS"],
+      imageUrl: "/projectsImages/sharaf.png",
+      link: "https://almahaqeri.vercel.app/",
     },
     {
-      title: 'To Russai',
-      description: 'Assists students applying to Russian universities by providing application guidance, visa help, and university selection',
-      technologies: ['React', 'Tailwind CSS'],
-      imageUrl: '/projectsImages/toru.png',
-      link: "https://toru.netlify.app/"
+      title: "To Russai",
+      description:
+        "Assists students applying to Russian universities by providing application guidance, visa help, and university selection",
+      technologies: ["React", "Tailwind CSS"],
+      imageUrl: "/projectsImages/toru.png",
+      link: "https://toru.netlify.app/",
     },
     {
-      title: 'Ali Alnono',
-      description: 'Showcases the journey of football player Ali Alnono from Yemen, highlighting his career, matches, and contributions.',
-      technologies: ['React', 'Tailwind CSS', 'Multimedia'],
-      imageUrl: '/projectsImages/alnono.png',
-      link: "https://alialnono.vercel.app/"
+      title: "Ali Alnono",
+      description:
+        "Showcases the journey of football player Ali Alnono from Yemen, highlighting his career, matches, and contributions.",
+      technologies: ["React", "Tailwind CSS", "Multimedia"],
+      imageUrl: "/projectsImages/alnono.png",
+      link: "https://alialnono.vercel.app/",
     },
     {
-      title: 'GaobaVid - YouTube Clone',
-      description: 'YouTube clone that replicates the core features of the original platform, allowing users to browse and watch videos',
-      technologies: ['React', 'Material UI', 'YouTube API'],
-      imageUrl: '/projectsImages/gaobavide.png',
-      link: "https://gaobavid.netlify.app/"
+      title: "GaobaVid - YouTube Clone",
+      description:
+        "krystallpoll is a business work offer a swimming pools and swimming clubs and their needs",
+      technologies: ["React", "Material UI", "YouTube API"],
+      imageUrl: "/projectsImages/gaobavide.png",
+      link: "https://gaobavid.netlify.app/",
+    },
+    {
+      title: "Protechies compony",
+      description:
+        "a leading web development company dedicated to transforming your ideas into innovative digital solutions.",
+      technologies: ["React", "Tailwind", "TypeScript API"],
+      imageUrl: "/projectsImages/protechies.png",
+      link: "https://protechies.netlify.app//",
+    },
+    {
+      title: "crystallpool compony",
+      description:
+        "YouTube clone that replicates the core features of the original platform, allowing users to browse and watch videos",
+      technologies: ["React", "Tailwind", "TypeScript"],
+      imageUrl: "/projectsImages/crystallpool.png",
+      link: "https://crystallpool.vercel.app/",
     },
   ];
 
